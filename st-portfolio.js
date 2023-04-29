e530c96b611fdb34837638d40d962c380c8e8098
Stefan Tampu <stefan_tampu@yahoo.com>
2023-04-29 15:58:51 -0400

    Change nav-button id; make right-side width = 25% when nav-button is clicked, or back to 0% when clicked again

---
+++ st-portfolio.js
@@ -0,0 +1,18 @@
+const leftSide = document.getElementById("left-side");
+const rightSide = document.getElementById("right-side");
+const navButton = document.getElementById("nav-button");
+
+let rightClick = 0;
+
+navButton.addEventListener("click", () => {
+    if (rightClick === 0){
+        rightClick = 1;
+        rightSide.style.width = "25%";
+        leftSide.style.width = "75%";
+    } else if (rightClick === 1){
+        rightClick = 0;
+        rightSide.style.width = "0%";
+        leftSide.style.width = "100%";
+    }
+    console.log("hello");
+})
