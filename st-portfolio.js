e1a1c8a5375ab95c259d0b32a28e368c915d2a19
Stefan Tampu <stefan_tampu@yahoo.com>
2023-12-07 11:21:06 +0200

    Create nv-active, ls-active & rs-active classes These are toggled to nav-button, left-side and right-side when nav-button is clicked nav-button, left-side and right-side are now classes, not ids simplify navButton event listener in js to adapt to new situation

--- st-portfolio.js
+++ st-portfolio.js
@@ -1,23 +1,13 @@
-const leftSide = document.getElementById("left-side");
-const rightSide = document.getElementById("right-side");
-const navButton = document.getElementById("nav-button");
+const leftSide = document.querySelector(".left-side");
+const rightSide = document.querySelector(".right-side");
+const navButton = document.querySelector(".nav-button");
 const projectBoxes = document.querySelectorAll(".project-box");
 const d5 = document.getElementById("d-5");
 
-let rightClick = 0;
-
 navButton.addEventListener("click", () => {
-    if (rightClick === 0){
-        navButton.style.transform = "rotate(0deg)";
-        rightClick = 1;
-        rightSide.style.width = "25%";
-        leftSide.style.width = "75%";
-    } else if (rightClick === 1){
-        navButton.style.transform = "rotate(45deg)";
-        rightClick = 0;
-        rightSide.style.width = "0%";
-        leftSide.style.width = "100%";
-    }
+    navButton.classList.toggle("nv-active");
+    rightSide.classList.toggle("rs-active");
+    leftSide.classList.toggle("ls-active");
 })
 
 for(let projectBox of projectBoxes){
